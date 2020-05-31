4061709850c265d9178507375b77f5aa24118ff4
chrisfra <chrisfra.pires@gmail.com>
2020-05-31 03:34:33 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -11,23 +11,25 @@ bot.on('ready', function() {
 bot.login(process.env.TOKEN);
 
 bot.on('message', message => {
-    if (message.content === prefix + "help"){
-        message.channel.send("Liste des commandes : \n - /help");
-    }
 
     if (message.content === "Salut"){
         message.reply("Bien le bonjour mon petit Muka's ! ^-^");
         console.log("Commande Salut effectuée");
     }
-    
-    if (message.content === prefix + "embed"){
+
+    if (message.content === prefix + "help"){
         var embed = new Discord.MessageEmbed()
-            .setTitle("EMBED")
-            .setDescription("Ceci est un embed")
+            .setTitle("Commandes d'aide")
+            .setDescription("Tu peux retrouver ci-dessous la liste des commandes principales de l'amiral Kizaru.")
+            .addFields(
+                { name: '/help', value: 'Permet d\'afficher la liste des commandes ', inline: true },
+                { name: 'Saluer', value: 'Le bot vous salue quand vous dites bonjour ! '}
+            )
             .addField(".help","Page d'aide", true)
             .addField("Embed01", "Embed 01 ! ;) Suivez moi sur [INSTAGRAM](https://www.instagram.com/japanim_dream/)", true)
             .setColor("0xFF8000")
             .setFooter("Bon moment parmis les muka's !")
         message.channel.send(embed);
     }
+
 });
