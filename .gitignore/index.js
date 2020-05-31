d5b13d1069f3b80e9657d53b2c905525e7131c04
chrisfra <chrisfra.pires@gmail.com>
2020-05-31 23:51:59 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -78,13 +78,4 @@ bot.on('message', message => {
         }
     }
 
-    /////////////////////////////////////// COMMANDE AVATAR ///////////////////////////
-    if (message.content === prefix + "avatar") {
-        const user = message.mentions.users.first() || message.author;
-        const avatarEmbed = new Discord.RichEmbed()
-            .setColor(0x333333)
-            .setAuthor(user.username)
-            .setImage(user.avatarURL);
-        message.channel.send(avatarEmbed);
-    }
 });
