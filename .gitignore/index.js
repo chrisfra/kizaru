ad2838dd5fa5992ef99f039581c8f41b8fca8ac2
chrisfra <chrisfra.pires@gmail.com>
2020-05-31 03:54:58 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,5 +1,12 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+const adapter = new FileSync('database.json');
+const db = low(adapter);
+
+db.defaults({ histoires: [], xp: [] }).write();
 
 var prefix = ("/");
 
@@ -8,6 +15,7 @@ bot.on('ready', function() {
     console.log("Connected");
 });
 
+//bot.login("NzE2NDQzODcwMDYxMjY0OTE3.XtMDWw.byaNy9kvMY2JOmV5Y_FhNNebH4Q");
 bot.login(process.env.TOKEN);
 
 bot.on('message', message => {
@@ -25,11 +33,38 @@ bot.on('message', message => {
                 { name: '/help', value: 'Permet d\'afficher la liste des commandes ', inline: true },
                 { name: 'Saluer', value: 'Le bot vous salue quand vous dites bonjour ! '}
             )
-            .addField(".help","Page d'aide", true)
-            .addField("Embed01", "Embed 01 ! ;) Suivez moi sur [INSTAGRAM](https://www.instagram.com/japanim_dream/)", true)
+            .addField("Aide supplémentaire","Contacter un admin", true)
+            .addField("Japanim", "Suivez moi sur [INSTAGRAM](https://www.instagram.com/japanim_dream/)", true)
             .setColor("0xFF8000")
-            .setFooter("Bon moment parmis les muka's !")
+            .setFooter("Passez un bon moment parmis les muka's !")
         message.channel.send(embed);
+    }
+
+    var msgauthor = message.author.id;
+
+    if(message.author.bot)return;
+
+    if(!db.get("xp").find({user: msgauthor}).value()){
+        db.get("xp").push({user: msgauthor, xp: 1}).write();
+    }else{
+        var userxpdb = db.get("xp").filter({user: msgauthor}).find('xp').value();
+        console.log(userxpdb);
+        var userxp = Object.values(userxpdb);
+        console.log(userxp)
+        console.log(`Nombre d'xp: ${userxp[1]}`)
+
+        db.get("xp").find({user: msgauthor}).assign({user: msgauthor, xp: userxp[1] += 1 }).write();
+
+        if (message.content === prefix + "xp"){
+            var xp = db.get("xp").filter({user: msgauthor}).find("xp").value();
+            var xpfinal = Object.values(xp);
+            var xp_embed = new Discord.MessageEmbed()
+                .setTitle(`XP de ${message.author.username}`)
+                .setColor('#F4D03F')
+                .setDescription("Affichage de l'xp")
+                .addField("XP: ", `${xpfinal[1]} xp`)
+            message.channel.send({embed: xp_embed});
+        }
     }
 
 });
