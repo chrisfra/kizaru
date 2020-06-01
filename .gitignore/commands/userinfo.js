161671f76582b856a3d8c0f1ede160db6414f17d
chrisfra <chrisfra.pires@gmail.com>
2020-06-01 02:07:27 +0200

    Update userinfo.js

--- .gitignore/commands/userinfo.js
+++ .gitignore/commands/userinfo.js
@@ -2,7 +2,6 @@ module.exports = {
     name: 'userinfo',
     description: 'Renvoie les informations d\'un utilisateur mentionné.',
     execute(message, args){
-        console.log("CA MARCHE PAAAAS");
         const user_mention = message.mentions.users.first();
         if(!user_mention){
             message.channel.send(`Voici les infos de ${message.author}`)
