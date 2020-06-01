6a4003d36081070e9674b898e22703346e92144c
chrisfra <chrisfra.pires@gmail.com>
2020-06-01 02:03:45 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const { Client, Collection } = require('discord.js');
+const { Client, Collection} = require('discord.js');
 const { TOKEN, PREFIX } = require('./config');
 
 const bot = new Client();
@@ -23,7 +23,7 @@ bot.on('message', message => {
 
     if (!message.content.startsWith(PREFIX) || message.author.bot) return;
 
-    const args = message.content.slice(PREFIX.length).split(/ + /);
+    const args = message.content.slice(PREFIX.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
     if(!bot.commands.has(command)) return;
@@ -43,23 +43,6 @@ bot.on('message', message => {
     // Affiche les infos du serveur //
     if (message.content.startsWith(`${PREFIX}serveur`)){
         message.channel.send(`Vous êtes sur le serveur ${message.guild.name}`);
-    }
-
-    //////////////////////////////////////// COMMANDE HELP ///////////////////////////////////
-    if (message.content === prefix + "help"){
-        var embed = new Discord.MessageEmbed()
-            .setTitle("Commandes d'aide")
-            .setDescription("Tu peux retrouver ci-dessous la liste des commandes principales de l'amiral Kizaru.")
-            .addFields(
-                { name: '/help', value: 'Permet d\'afficher la liste des commandes ', inline: true },
-                { name: '/xp', value: 'Permet d\'afficher ton xp', inline: true },
-                { name: 'Saluer', value: 'Le bot vous salue quand vous dites bonjour ! '}
-            )
-            .addField("Aide supplémentaire","Contacter un admin", true)
-            .addField("Japanim", "Suivez moi sur [INSTAGRAM](https://www.instagram.com/japanim_dream/)", true)
-            .setColor("#FF8000")
-            .setFooter("Passez un bon moment parmis les muka's !")
-        message.channel.send(embed);
     }
 
 });
